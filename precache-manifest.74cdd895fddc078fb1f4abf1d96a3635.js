8255b40b0ef2bbf0430129fc823eedb40d4c5276
Xinyun Zhao <yocchiroja20@gmail.com>
2020-09-20 07:17:22 -0400

    Updates

---
+++ precache-manifest.74cdd895fddc078fb1f4abf1d96a3635.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5905b842a04f1f683f6ea61daa50bc57",
+    "url": "/ssi-exchange-rate-calculator/index.html"
+  },
+  {
+    "revision": "a7bf90ff2ced72c60998",
+    "url": "/ssi-exchange-rate-calculator/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "70aab53825237d4f4058",
+    "url": "/ssi-exchange-rate-calculator/static/css/main.5b3a17c8.chunk.css"
+  },
+  {
+    "revision": "a7bf90ff2ced72c60998",
+    "url": "/ssi-exchange-rate-calculator/static/js/2.547132ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ssi-exchange-rate-calculator/static/js/2.547132ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70aab53825237d4f4058",
+    "url": "/ssi-exchange-rate-calculator/static/js/main.186ab6e8.chunk.js"
+  },
+  {
+    "revision": "97b08cb46029d87e0674",
+    "url": "/ssi-exchange-rate-calculator/static/js/runtime-main.aafc6cf9.js"
+  },
+  {
+    "revision": "8ebfab8f167e6723aff8a3646414f245",
+    "url": "/ssi-exchange-rate-calculator/static/media/money.8ebfab8f.png"
+  }
+]);
